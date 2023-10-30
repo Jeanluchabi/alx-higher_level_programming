3a288c5d25d93da96b72edeb37565ab81387b194
Jean Luc Habimana <jeanluchabi@gmail.com>
2023-10-30 23:57:34 +0200

    python by Luc

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * check_cycle - This function checks of a singly-linked list contains a cycle.
+ * @list: The name of singly-linked list
+ *
+ * Return: 0 if there is no cycle
+ *         1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *weak, *quick;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	weak = list->next;
+	quick = list->next->next;
+
+	while (weak && quick && quick quick->next)
+	{
+		if (weak == quick)
+			return (1);
+
+		weak = weak->next;
+		quick = quick->next->next;
+	}
+	return (0);
