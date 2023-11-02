c2e4888a7d7c947cdaf319bdc1963a6477b098cc
Jean Luc Habimana <jeanluchabi@gmail.com>
2023-11-02 14:26:52 +0200

    Luc_python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     a = 10
     b = 5
 
-    print(f"{} + {} = {}".format(a, b, add(a, b)))
-    print(f"{} - {} = {}".format(a, b, subtract(a, b)))
-    print(f"{} * {} = {}".format(a, b, multiply(a, b)))
-    print(f"{} / {} = {}".format(a, b, divide(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, subtract(a, b)))
+    print("{} * {} = {}".format(a, b, multiply(a, b)))
+    print("{} / {} = {}".format(a, b, divide(a, b)))
