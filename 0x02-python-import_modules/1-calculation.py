b67b37d68896e74b51d788e3489737a2fc355e2c
Jean Luc Habimana <jeanluchabi@gmail.com>
2023-11-02 13:54:31 +0200

    Luc_python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-a = 10
-b = 5
+if __name__ == '__main__':
+    from calculator_1 import add, subtract, multiply, divide
+    
+    a = 10
+    b = 5
 
-from calculator_1 import add, subtract, multiply, divide
-
-result_ad = add(a, b)
-result_sub = subtract(a, b)
-result_mul = multiply(a, b)
-result_div = divide(a, b)
-
-print(f"{a} + {b} = {result_ad}")
-print(f"{a} - {b} = {result_sub}")
-print(f"{a} * {b} = {result_mul}")
-print(f"{a} / {b} = {result_div}")
+    print(f"{} + {} = {}".format(a, b, add(a, b)))
+    print(f"{} - {} = {}".format(a, b, subtract(a, b)))
+    print(f"{} * {} = {}".format(a, b, multiply(a, b)))
+    print(f"{} / {} = {}".format(a, b, divide(a, b)))
