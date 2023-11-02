ddd263a5ebc57772381c8dff445443e875d658b8
Jean Luc Habimana <jeanluchabi@gmail.com>
2023-11-02 14:48:20 +0200

    Luc_python

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys, math
+    
+    result = 0
+
+    for n in sys.argv:
+        result += int(n)
+        print("{}".fomat(result))
+
+
