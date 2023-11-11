e1114143fc1e0108bbc5ec205765b56d45753638
Jean Luc HABIMANA <jeanluchabi@gmail.com>
2023-11-11 12:59:13 -0800

    Luc_python

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def magic_cslculation(a, b):
-    add = __import__('magic_calculation_102').add
-    sub = __import__('magic_calculation_102').sub
+    from magic_calculation_102 import add, sub
 
     if a < b:
-        c = add(a, b)
+        result = add(a, b)
         for n in range(4, 6):
-            c = add(c, n)
-        return c
+            result = add(result, n)
+        return result
     else:
         return sub(a, b)
