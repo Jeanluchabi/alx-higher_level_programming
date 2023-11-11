9387f85649204da8e712fc4c533911b5739944c7
Jean Luc HABIMANA <jeanluchabi@gmail.com>
2023-11-11 12:37:22 -0800

    Luc_python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, subtract, multiply, divide
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+
+    x = int(sys.argv[1])
+    operator = sys.argv[2]
+    y = int(sys.argv[3])
+
+    if operator == '+':
+        result = add(x, y)
+    elif operator == '-':
+        result = subtract(x, y)
+    elif operator == '*':
+        result = multiply(x, y)
+    elif operator == '/':
+        result = divide(x, y)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print("{} {} {} = {}".format(x, operator, y, result))
